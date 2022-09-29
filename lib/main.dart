84f2ba06f30941d0a20d6675e0544b470b8a7ef4
Jody-septiawan <jody.septiawan5@gmail.com>
2022-09-29 11:16:15 +0700

    State Management Multi Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:dumbcation/provider/color_provider.dart';
+import 'dart:ui';
+import 'package:dumbcation/provider/money_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'provider/color_provider.dart';
+import 'provider/cart_provider.dart';
+import 'provider/money_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,25 +13,86 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: 'Poppins'),
-        home: Scaffold(
-          body: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Test"),
-              Container(
-                child: Align(alignment: Alignment.centerRight),
-                height: 30,
-                width: 150,
-                margin: EdgeInsets.all(5),
-                padding: EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  color: Colors.purple[100],
-                  border: Border.all(color: Colors.purple, width: 2),
+        home: MultiProvider(
+          providers: [
+            ChangeNotifierProvider<Money>(
+              create: (context) => Money(),
+            ),
+            ChangeNotifierProvider<Cart>(
+              create: (context) => Cart(),
+            ),
+          ],
+          child: Scaffold(
+            floatingActionButton: Consumer<Money>(
+              builder: (context, money, child) => Consumer<Cart>(
+                builder: (context, cart, child) => FloatingActionButton(
+                  onPressed: () {
+                    cart.quantity += 1;
+                    money.balance -= 500;
+                  },
+                  child: Icon(Icons.add_shopping_cart),
+                  backgroundColor: Colors.yellow,
                 ),
-              )
-            ],
-          )),
+              ),
+            ),
+            body: SafeArea(
+                child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Wallet"),
+                      Container(
+                        child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Consumer<Money>(
+                              builder: (context, value, child) => Text(
+                                value.balance.toString(),
+                                style: TextStyle(color: Colors.purple),
+                              ),
+                            )),
+                        height: 30,
+                        width: 150,
+                        margin: EdgeInsets.all(5),
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.purple[100],
+                            border: Border.all(color: Colors.purple, width: 2),
+                            borderRadius: BorderRadius.circular(5)),
+                      )
+                    ],
+                  ),
+                  Container(
+                    child: Consumer<Cart>(
+                      builder: (context, value, child) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Mango (500) x " + value.quantity.toString(),
+                            style: TextStyle(color: Colors.green),
+                          ),
+                          Text(
+                            (500 * value.quantity).toString(),
+                            style: TextStyle(color: Colors.green),
+                          )
+                        ],
+                      ),
+                    ),
+                    height: 30,
+                    width: 300,
+                    margin: EdgeInsets.all(5),
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: Colors.green[100],
+                        border: Border.all(color: Colors.green, width: 2),
+                        borderRadius: BorderRadius.circular(5)),
+                  )
+                ],
+              ),
+            )),
+          ),
         ));
   }
 }
